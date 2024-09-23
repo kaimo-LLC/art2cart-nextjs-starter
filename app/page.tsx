20992223f7000d14f4beb39163a65273c3acda04
Nicholas Culpin <njculpin@gmail.com>
2024-09-22 22:17:12 -0700

    fix env

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 import { catalogSdk } from "./lib/art2cart";
 import { CatalogProduct } from "art2cart";
 
-export default async function Home() {
+async function fetchProducts() {
   let { data } = await catalogSdk.getByTeam();
-
   if (!data) {
-    return <></>;
+    return [];
   }
+  return data;
+}
 
+export default async function Home() {
+  const products = await fetchProducts();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <ul>
-        {data.map((post: CatalogProduct) => (
+        {products.map((post: CatalogProduct) => (
           <li key={post.id}>{post.code}</li>
         ))}
       </ul>
