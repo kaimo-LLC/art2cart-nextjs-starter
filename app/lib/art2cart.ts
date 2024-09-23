66ad8759b5f50d5165b0eba0ed76a4145150d9e7
Nicholas Culpin <njculpin@gmail.com>
2024-09-22 21:23:27 -0700

    import sdks

--- app/lib/art2cart.ts
+++ app/lib/art2cart.ts
@@ -1,4 +1,26 @@
-import { Configuration, CatalogProductsApi } from "art2cart";
+import {
+  Configuration,
+  CatalogProductsApi,
+  DesignsApi,
+  ChannelsApi,
+  ListingsApi,
+  JobsApi,
+  PrintersApi,
+  PrintFilesApi,
+  PreviewImagesApi,
+  PersonalizationApi,
+  ImpositionApi,
+} from "art2cart";
 
 const config = new Configuration({ apiKey: process.env.ART2CART });
+
+export const jobSdk = new JobsApi(config);
 export const catalogSdk = new CatalogProductsApi(config);
+export const channelSdk = new ChannelsApi(config);
+export const designSdk = new DesignsApi(config);
+export const listingSdk = new ListingsApi(config);
+export const printerSdk = new PrintersApi(config);
+export const printFileSdk = new PrintFilesApi(config);
+export const previewImageSdk = new PreviewImagesApi(config);
+export const personalizationSdk = new PersonalizationApi(config);
+export const impositionSdk = new ImpositionApi(config);
